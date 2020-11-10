ba4ace83a902c6bbf22abb1976fdc3f27e9c1c1e
Ilya Stakheev <istaheev@gmail.com>
2020-11-10 14:22:36 +0100

    Add Warnsdorff's rule check

--- main.go
+++ main.go
@@ -1,91 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 type position struct {
 	x int
 	y int
 }
 
-type stackOp struct {
-	moveNum int
-	pos     position
-}
-
-// stack implements simple stack of operations
-type stack struct {
-	items []stackOp
-	count int
-}
-
-func newStack() stack {
-	return stack{
-		items: make([]stackOp, 0),
-		count: 0,
-	}
-}
-
-func (s *stack) push(op stackOp) {
-	if s.count == len(s.items) {
-		s.items = append(s.items, op)
-	} else {
-		s.items[s.count] = op
-	}
-
-	s.count++
-}
-
-func (s *stack) pop() (stackOp, bool) {
-	if s.count == 0 {
-		return stackOp{}, false
-	}
-
-	s.count--
-	return s.items[s.count], true
-}
-
-// Predefined list of possible relative jump directions
-
-var jumpDirections1 = []position{
-	// horizontal
-	position{x: 3, y: 0},
-	position{x: -3, y: 0},
-	// vertical
-	position{x: 0, y: 3},
-	position{x: 0, y: -3},
-	// diagonal
-	position{x: 2, y: 2},
-	position{x: -2, y: 2},
-	position{x: 2, y: -2},
-	position{x: -2, y: -2},
-}
-
-var jumpDirections2 = []position{
-	// horizontal
-	position{x: 1, y: 0},
-	position{x: -1, y: 0},
-	// vertical
-	position{x: 0, y: 1},
-	position{x: 0, y: -1},
+// move point by specified offset
+func (p position) move(dir position) position {
+	return position{x: p.x + dir.x, y: p.y + dir.y}
 }
 
 type solver struct {
-	width   int
-	height  int
+	// Board size and available directions (read-only)
+	width      int
+	height     int
+	directions []position
+	// Tracks visited cells
 	visited []bool
-	moves   []position
+	// A list of moves forming current path
+	moves []position
 }
 
-func newSolver(width, height int) solver {
+func newSolver(width, height int, directions []position) solver {
 	var boardSize = width * height
 	return solver{
-		width:   width,
-		height:  height,
-		visited: make([]bool, boardSize),
-		moves:   make([]position, 0, boardSize),
+		width:      width,
+		height:     height,
+		directions: directions,
+		visited:    make([]bool, boardSize),
+		moves:      make([]position, 0, boardSize),
 	}
 }
 
+// cellIndex returns a zero-based index of the cell to be used as an index in
+// other arrays. Index is always in [0, boardSize) range.
 func (s *solver) cellIndex(p position) int {
 	return p.y*s.width + p.x
 }
@@ -119,7 +72,8 @@ func (s *solver) jump(p position) bool {
 	return true
 }
 
-func (s *solver) backtrack(move int) {
+// undoMoves marks all the moves in the range [move..] as if they didn't happen
+func (s *solver) undoMoves(move int) {
 	for i := move; i < len(s.moves); i++ {
 		var p = s.moves[i]
 		s.setVisited(p, false)
@@ -128,46 +82,93 @@ func (s *solver) backtrack(move int) {
 }
 
 func (s *solver) findPath(start position) []position {
-	var stack = newStack()
+	// To avoid recursion the function uses its own heap-based stack.
+	// An entry holds a move to do and its sequential number in the overall path
+	type stackOp struct {
+		moveNum int
+		pos     position
+	}
 
-	stack.push(stackOp{moveNum: 0, pos: start})
+	var stack = make([]stackOp, 0)
+
+	// Setup initial move
+	stack = append(stack, stackOp{moveNum: 0, pos: start})
 
 	for {
-		op, ok := stack.pop()
-		if !ok {
-			// stack is empty
+		if len(stack) == 0 {
+			// No more next moves
 			break
 		}
 
-		// fmt.Printf("Move: %d, Prev: %d, Pos: %v\n", op.moveNum, prevMoveNum, op.pos)
+		// Pop the next move to try
+		var op = stack[len(stack)-1]
+		stack = stack[0 : len(stack)-1]
 
+		// Handle the backtrack situation when the current branch didn't
+		// generate any path and we have to go several moves back
 		if op.moveNum < len(s.moves) {
-			s.backtrack(op.moveNum)
+			s.undoMoves(op.moveNum)
 		}
 
 		if !s.jump(op.pos) {
 			continue
 		}
 
+		// We found a path when jumped over every cell on the board
 		if len(s.moves) == s.boardSize() {
 			return s.moves
 		}
 
-		for _, dir := range jumpDirections1 {
-			var newPos = position{x: op.pos.x + dir.x, y: op.pos.y + dir.y}
-			if s.canJump(newPos) {
-				stack.push(stackOp{moveNum: op.moveNum + 1, pos: newPos})
-			}
+		// Otherwise queue potential moves from the current cell
+		for _, newPos := range s.getAvailableMoves(op.pos) {
+			stack = append(stack, stackOp{moveNum: op.moveNum + 1, pos: newPos})
 		}
 	}
 
 	return nil
 }
 
+// getAvailableMoves returns a list of proper moves which can be made from
+// the specified cell.
+func (s *solver) getAvailableMoves(p position) []position {
+	var moves = make([]position, 0, len(s.directions))
+	for _, dir := range s.directions {
+		var newPos = p.move(dir)
+		if s.canJump(newPos) {
+			moves = append(moves, newPos)
+		}
+	}
+
+	// Apply Warnsdorff's rule: the move with the least potential next moves
+	// should be handled first.
+	// Note: since available moves are processed from the highest index to
+	// the lowest one they are sorted by descending order of available moves
+	// count.
+	var movesCount = make([]int, len(moves))
+	for i := range moves {
+		movesCount[i] = s.getPotentialMovesCount(moves[i])
+	}
+
+	sort.Slice(moves, func(i, j int) bool { return movesCount[i] > movesCount[j] })
+	return moves
+}
+
+// getPotentialMovesCount returns amount of proper moves which can be done
+// from the specified cell
+func (s *solver) getPotentialMovesCount(p position) int {
+	var count = 0
+	for _, dir := range s.directions {
+		if s.canJump(p.move(dir)) {
+			count++
+		}
+	}
+	return count
+}
+
 func validatePath(width, height int, moves []position) bool {
-	var solver = newSolver(width, height)
-	// len(moves) == 0 could happen if board.size() == 0
-	if len(moves) == 0 || len(moves) != solver.boardSize() {
+	var solver = newSolver(width, height, jumpDirections)
+
+	if len(moves) != solver.boardSize() {
 		return false
 	}
 
@@ -181,12 +182,28 @@ func validatePath(width, height int, moves []position) bool {
 	return true
 }
 
-func main() {
-	var width = 8
-	var height = 8
-	var start = position{x: 0, y: 0}
+// Predefined list of possible relative jump directions
 
-	var solver = newSolver(width, height)
+var jumpDirections = []position{
+	// horizontal
+	position{x: 3, y: 0},
+	position{x: -3, y: 0},
+	// vertical
+	position{x: 0, y: 3},
+	position{x: 0, y: -3},
+	// diagonal
+	position{x: 2, y: 2},
+	position{x: -2, y: 2},
+	position{x: 2, y: -2},
+	position{x: -2, y: -2},
+}
+
+func main() {
+	var width = 10
+	var height = 10
+	var start = position{x: 1, y: 0}
+
+	var solver = newSolver(width, height, jumpDirections)
 	var path = solver.findPath(start)
 	if path != nil {
 		fmt.Println("Path found")
