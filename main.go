cc33f13cd66cc02efa83680acb783f219bc12ccc
Ilya Stakheev <istaheev@gmail.com>
2020-11-10 10:18:08 +0100

    Refactor the code a bit

--- main.go
+++ main.go
@@ -7,23 +7,6 @@ type position struct {
 	y int
 }
 
-type board struct {
-	width  int
-	height int
-}
-
-func (b board) cellIndex(p position) int {
-	return p.y*b.width + p.x
-}
-
-func (b board) inside(p position) bool {
-	return p.x >= 0 && p.x < b.width && p.y >= 0 && p.y < b.height
-}
-
-func (b board) size() int {
-	return b.width * b.height
-}
-
 type stackOp struct {
 	moveNum int
 	pos     position
@@ -62,8 +45,8 @@ func (s *stack) pop() (stackOp, bool) {
 }
 
 // Predefined list of possible relative jump directions
-/*
-var jumpDirections = []position{
+
+var jumpDirections1 = []position{
 	// horizontal
 	position{x: 3, y: 0},
 	position{x: -3, y: 0},
@@ -76,26 +59,77 @@ var jumpDirections = []position{
 	position{x: 2, y: -2},
 	position{x: -2, y: -2},
 }
-*/
-var jumpDirections = []position{
+
+var jumpDirections2 = []position{
 	// horizontal
-	position{x: 2, y: 0},
-	position{x: -2, y: 0},
+	position{x: 1, y: 0},
+	position{x: -1, y: 0},
 	// vertical
-	position{x: 0, y: 2},
-	position{x: 0, y: -2},
-	// diagonal
-	position{x: 1, y: 1},
-	position{x: -1, y: -1},
+	position{x: 0, y: 1},
+	position{x: 0, y: -1},
 }
 
-func findPath(board board, start position) []position {
-	// Prepare required data structures
-	var visited = make([]bool, board.size())
-	var moves = make([]position, board.size())
+type solver struct {
+	width   int
+	height  int
+	visited []bool
+	moves   []position
+}
+
+func newSolver(width, height int) solver {
+	var boardSize = width * height
+	return solver{
+		width:   width,
+		height:  height,
+		visited: make([]bool, boardSize),
+		moves:   make([]position, 0, boardSize),
+	}
+}
+
+func (s *solver) cellIndex(p position) int {
+	return p.y*s.width + p.x
+}
+
+func (s *solver) inside(p position) bool {
+	return p.x >= 0 && p.x < s.width && p.y >= 0 && p.y < s.height
+}
+
+func (s *solver) boardSize() int {
+	return s.width * s.height
+}
+
+func (s *solver) isVisited(p position) bool {
+	return s.visited[s.cellIndex(p)]
+}
+
+func (s *solver) setVisited(p position, visited bool) {
+	s.visited[s.cellIndex(p)] = visited
+}
+
+func (s *solver) canJump(p position) bool {
+	return s.inside(p) && !s.isVisited(p)
+}
+
+func (s *solver) jump(p position) bool {
+	if !s.canJump(p) {
+		return false
+	}
+	s.moves = append(s.moves, p)
+	s.setVisited(p, true)
+	return true
+}
+
+func (s *solver) backtrack(move int) {
+	for i := move; i < len(s.moves); i++ {
+		var p = s.moves[i]
+		s.setVisited(p, false)
+	}
+	s.moves = s.moves[0:move]
+}
+
+func (s *solver) findPath(start position) []position {
 	var stack = newStack()
 
-	var prevMoveNum = 0
 	stack.push(stackOp{moveNum: 0, pos: start})
 
 	for {
@@ -105,70 +139,61 @@ func findPath(board board, start position) []position {
 			break
 		}
 
-		//fmt.Printf("Move: %d, Prev: %d, Pos: %v\n", op.moveNum, prevMoveNum, op.pos)
+		// fmt.Printf("Move: %d, Prev: %d, Pos: %v\n", op.moveNum, prevMoveNum, op.pos)
 
-		if op.moveNum <= prevMoveNum {
-			for i := op.moveNum; i <= prevMoveNum; i++ {
-				var p = moves[i]
-				visited[board.cellIndex(p)] = false
-			}
+		if op.moveNum < len(s.moves) {
+			s.backtrack(op.moveNum)
 		}
 
-		var cellIdx = board.cellIndex(op.pos)
-		if !board.inside(op.pos) || visited[cellIdx] {
+		if !s.jump(op.pos) {
 			continue
 		}
 
-		visited[cellIdx] = true
-		moves[op.moveNum] = op.pos
-		prevMoveNum = op.moveNum
-
-		if op.moveNum == board.size()-1 {
-			return moves
+		if len(s.moves) == s.boardSize() {
+			return s.moves
 		}
 
-		for _, dir := range jumpDirections {
+		for _, dir := range jumpDirections1 {
 			var newPos = position{x: op.pos.x + dir.x, y: op.pos.y + dir.y}
-			if !board.inside(newPos) || visited[board.cellIndex(newPos)] {
-				continue
+			if s.canJump(newPos) {
+				stack.push(stackOp{moveNum: op.moveNum + 1, pos: newPos})
 			}
-
-			stack.push(stackOp{moveNum: op.moveNum + 1, pos: newPos})
 		}
 	}
 
 	return nil
 }
 
-func validatePath(board board, moves []position) bool {
+func validatePath(width, height int, moves []position) bool {
+	var solver = newSolver(width, height)
 	// len(moves) == 0 could happen if board.size() == 0
-	if len(moves) == 0 || len(moves) != board.size() {
+	if len(moves) == 0 || len(moves) != solver.boardSize() {
 		return false
 	}
 
-	var visited = make([]bool, board.size())
-
 	for _, m := range moves {
-		if visited[board.cellIndex(m)] {
+		if !solver.canJump(m) {
 			return false
 		}
-		visited[board.cellIndex(m)] = true
+		solver.setVisited(m, true)
 	}
 
 	return true
 }
 
 func main() {
-	var board = board{width: 8, height: 8}
+	var width = 8
+	var height = 8
 	var start = position{x: 0, y: 0}
 
-	var path = findPath(board, start)
+	var solver = newSolver(width, height)
+	var path = solver.findPath(start)
 	if path != nil {
 		fmt.Println("Path found")
 		for i := range path {
 			fmt.Printf("%v\n", path[i])
 		}
-		var valid = validatePath(board, path)
+		var valid = validatePath(width, height, path)
 		fmt.Printf("validate: %v", valid)
 	} else {
 		fmt.Println("Path not found")
